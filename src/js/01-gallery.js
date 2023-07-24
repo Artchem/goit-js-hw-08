d725fc31dc9a1d00e90c88ae8b808b733356bac0
Artchem <chemerys.artem@gmail.com>
2023-07-24 23:32:02 +0300

    update 01-gallary.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,32 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryRef = document.querySelector('.gallery');
+const markupGallery = makeMarkupFromArray(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', markupGallery);
+
+function makeMarkupFromArray(gallery) {
+  return gallery
+    .map(({ preview, original, description }) => {
+      return `
+    <li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+            <img
+              class="gallery__image"
+              src="${preview}"
+              alt="${description}"
+            />
+          </a>
+    </li>
+    `;
+    })
+    .join('');
+}
+
+let gallery = new SimpleLightbox('.gallery__link', { captionsData: 'alt' });
