ef519e560dbfee4bde740f68097c0eb6e804ac55
Artchem <chemerys.artem@gmail.com>
2023-07-29 15:28:18 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
 
+// console.log(localStorage);
+
+// localStorage.setItem('my-name', JSON.stringify({ name: 'Artem', age: 36 }));
+
+// const saveData = localStorage.getItem('my-name');
+// console.log(saveData);
+
+// const parsedData = JSON.parse(saveData);
+// console.log(parsedData);
+// localStorage.removeItem('my-name');
